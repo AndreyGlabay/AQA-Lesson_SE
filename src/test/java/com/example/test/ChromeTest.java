40987795e90429dc6d763ca6370c736b7ae5527f
Andriy Glabay <rishelevsky@gmail.com>
2024-02-14 19:11:13 +0200

    implements website visiting with the driver.

--- src/test/java/com/example/test/ChromeTest.java
+++ src/test/java/com/example/test/ChromeTest.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ChromeTest {
-    String baseUrl = "https://demoqa.com/";
+    String baseUrl = "https://demoqa.com/"; // (1.c) Implements new var with URL for website visiting.
     @Test
-    public void testChrome() {
-        WebDriver driver = new ChromeDriver();
+    public void testChrome() { // (1.a) implement annotation @Test with test method testChrome().
+        WebDriver driver = new ChromeDriver(); // (1.b) implements new driver with WebDriver.
+        driver.get(baseUrl); // (1.d) implements website visiting with the driver.
+        // var title = driver.getTitle();
+
 
     }
 }
