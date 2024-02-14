fe707f920cc24a5036e5c6affa25d39755a41a6e
Andriy Glabay <rishelevsky@gmail.com>
2024-02-14 19:03:47 +0200

    ChromeTest @Test testChrome(): implements new driver with WebDriver

--- src/test/java/com/example/test/ChromeTest.java
+++ src/test/java/com/example/test/ChromeTest.java
@@ -1,10 +1,13 @@
 package com.example.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ChromeTest {
     @Test
     public void testChrome() {
+        WebDriver driver = new ChromeDriver();
 
     }
 }
