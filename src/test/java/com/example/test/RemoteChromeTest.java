adf26b0632d1a57a16a339a7e344ecb227fd8680
Andriy Glabay <rishelevsky@gmail.com>
2024-02-14 23:20:00 +0200

    RemoteChromeTest: Initiate options var with ChromeOptions();

--- src/test/java/com/example/test/RemoteChromeTest.java
+++ src/test/java/com/example/test/RemoteChromeTest.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -9,11 +10,13 @@ import org.testng.annotations.Test;
 
 public class RemoteChromeTest { // (6.a) Copy test class ChromeTest and rename it to RemoteChromeTest
     String baseUrl = "https://demoqa.com/";
+    String gridUrl = "http://localhost:6789"; // (6.b) Initiate gridUrl var with HTTP + localhost + port num;
     WebDriver driver;
+    ChromeOptions options = new ChromeOptions(); // (6.c) Initiate options var with ChromeOptions();
 
     @BeforeTest
     public void driverOpening(){
-        driver = new ChromeDriver();
+
     }
 
     @AfterTest
