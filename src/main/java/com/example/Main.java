989c46397ea6c0127e8406823fbbcd60af532b82
Andriy Glabay <rishelevsky@gmail.com>
2024-02-14 18:32:51 +0200

    Project: change structure - change package from 'src/main/java/com.example.school.test' to the 'src/main/java/com.example'.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,4 @@
-package com.example.school.test;
+package com.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
