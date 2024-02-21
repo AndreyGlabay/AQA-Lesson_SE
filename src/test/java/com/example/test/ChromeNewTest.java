a65921d50b3474cada919fc7fd7e5b2ac1e2de98
Andriy Glabay <rishelevsky@gmail.com>
2024-02-21 20:02:52 +0200

    Test Class ChromeNewTest: (1) Declare and initiate vars for WebDriver, URL, Path, EP; (2) @BeforeTest: Implements Chrome instance opening; (3) @AfterTest: Implements Chrome instance closing; (4) @Test: Implements Web Page getting.

--- src/test/java/com/example/test/ChromeNewTest.java
+++ src/test/java/com/example/test/ChromeNewTest.java
@@ -1,4 +1,30 @@
 package com.example.test;
 
-public class ChromeNewTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ChromeNewTest {                            // (step 8) Create new test class;
+    WebDriver driver;                                   // (step 8.0) Declare var for WebDriver;
+    String urlRozetka = "https://rozetka.com.ua/";      // (step 8.0) Initiate var for URL;
+    String pathRozetksHome = "ua/";                     // (step 8.0) Initiate var for Path;
+    String homePage = urlRozetka + pathRozetksHome;     // (step 8.0) Initiate var for EP;
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new ChromeDriver();                    // (step 8.1) Implements Chrome instance opening;
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.close();                                 // (step 8.2) Implements Chrome instance closing;
+    }
+
+    @Test
+    public void findElementSet1() {
+        driver.get(homePage);                           // (step 8.3) Implements Web Page getting;
+    }
 }
