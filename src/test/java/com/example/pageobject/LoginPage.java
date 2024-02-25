edd0f3cdd2136563f35d424eae54c18c51515bc5
Andriy Glabay <rishelevsky@gmail.com>
2024-02-25 22:00:04 +0200

    (1) Test class LoginPage.java: implement var for web driver; (2) LoginPage.java: Create locators for input_login, input_password, button_login, error_message.; (3) LoginPage.java: Create method LoginPage(WebDriver driver); (4) LoginPage(WebDriver driver): Find web elements using locators.

--- src/test/java/com/example/pageobject/LoginPage.java
+++ src/test/java/com/example/pageobject/LoginPage.java
@@ -1,4 +1,26 @@
 package com.example.pageobject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+    private WebDriver driver; // (13.1) Implement var for web driver;
+    private static final String USERNAME_LOCATOR = "//*[@id=\"user-name\"]"; // (13.2) Create locator for Input UserName;
+    private static final String PASSWORD_LOCATOR = "//*[@id=\"password\"]"; // (13.2) Create locator for Input Password;
+    private static final String BUTTON_LOCATOR = "//*[@id=\"login-button\"]"; // (13.2) Create locator for Login button;
+    private static final String MESSAGE_LOCATOR = "//h3[@data-test=\"error\"]"; // (13.2) Add locator for the Error msg;
+
+    public LoginPage(WebDriver driver) { // (13.3) Create method LoginPage(WebDriver driver)
+        this.driver = driver;
+        userName = driver.findElement(By.xpath(USERNAME_LOCATOR)); // (13.4) Find web elements using locators.
+        password = driver.findElement(By.xpath(PASSWORD_LOCATOR));
+        button = driver.findElement(By.xpath(BUTTON_LOCATOR));
+        message = driver.findElement(By.xpath(MESSAGE_LOCATOR));
+    }
+
+    private WebElement userName;
+    private WebElement password;
+    private WebElement button;
+    private WebElement message;
 }
