ae4c6ccb5a64e71b32afa1400e1309bc1e6e512b
Andriy Glabay <rishelevsky@gmail.com>
2024-02-25 20:42:27 +0200

    Test class SwagLabsTest.java: create annotations @BeforeTest and using test method arrange start of web browser.

--- src/test/java/com/example/test/SwagLabsTest.java
+++ src/test/java/com/example/test/SwagLabsTest.java
@@ -1,4 +1,19 @@
 package com.example.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
 public class SwagLabsTest {
+    private static WebDriver driver;
+    @BeforeTest // (11.1) Arrange start of web browser;
+    public void startBrowser() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest // (11.2) Arrange stop of web browser;
+    public void stopBrowser() {
+        driver.close();
+    }
 }
