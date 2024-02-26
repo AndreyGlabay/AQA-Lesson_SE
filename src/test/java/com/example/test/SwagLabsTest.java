9012b12cbcc2b9f091d6c391f385b6ca33c95fb6
Andriy Glabay <rishelevsky@gmail.com>
2024-02-26 19:28:38 +0200

    SwagLabsTest.java - ShouldSeeBrokenLinks(): (1) Assert URL after success login. (2) Create var for the InventoryPage.

--- src/test/java/com/example/test/SwagLabsTest.java
+++ src/test/java/com/example/test/SwagLabsTest.java
@@ -32,7 +32,6 @@ public class SwagLabsTest {
         driver.get(baseUrl); // (11.4) Implement open web page GET URL using web driver;
         var loginPage = new LoginPage(driver); // (14.1) Arrange login flow using LoginPage Object.
         loginPage.login("standard_user", "secret_sauce"); // (14.1) Arrange login using LoginPage obj.
-        System.out.println(driver.getCurrentUrl());
         Assert.assertTrue(driver.getCurrentUrl().endsWith("inventory.html")); // (17.1) Assert URL after success login;
         var inventoryPage = new InventoryPage(driver); // (20.6) Create var for the InventoryPage;
         Assert.assertEquals(inventoryPage.getNumberOfInventoryItems(), 6); //(20.7)Assert qty of inventory items;
@@ -54,9 +53,8 @@ public class SwagLabsTest {
         driver.get(baseUrl); // (21.1) Implement open web page GET URL using web driver;
         var loginPage = new LoginPage(driver); // (21.2) Arrange login flow using LoginPage Object.
         loginPage.login("problem_user", "secret_sauce"); // (21.3) Arrange login using LoginPage obj.
-        System.out.println(driver.getCurrentUrl());
-        Assert.assertTrue(driver.getCurrentUrl().endsWith("inventory.html")); // (17.1) Assert URL after success login;
-        var inventoryPage = new InventoryPage(driver); // (20.6) Create var for the InventoryPage;
-        Assert.assertEquals(inventoryPage.getNumberOfInventoryItems(), 6); //(20.7)Assert qty of inventory items;
+        Assert.assertTrue(driver.getCurrentUrl().endsWith("inventory.html")); // (21.9) Assert URL after success login;
+        var inventoryPage = new InventoryPage(driver); // (21.10) Create var for the InventoryPage;
+        A
     }
 }
