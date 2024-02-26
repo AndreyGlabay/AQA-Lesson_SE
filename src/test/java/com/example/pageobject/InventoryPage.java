a344a8b9e968c7f9bb010eee2c927411ed81f733
Andriy Glabay <rishelevsky@gmail.com>
2024-02-26 18:03:28 +0200

    Package src/test/java/com.example/pageobject: copy and rename new test class: LoginPage.java -> InventoryPage.java.

---
+++ src/test/java/com/example/pageobject/InventoryPage.java
@@ -0,0 +1,37 @@
+package com.example.pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class InventoryPage {
+    private WebDriver driver; // (13.1) Implement var for web driver;
+    private static final String USERNAME_LOCATOR = "//*[@id=\"user-name\"]"; // (13.2) Create locator for Input UserName;
+    private static final String PASSWORD_LOCATOR = "//*[@id=\"password\"]"; // (13.2) Create locator for Input Password;
+    private static final String BUTTON_LOCATOR = "//*[@id=\"login-button\"]"; // (13.2) Create locator for Login button;
+    private static final String MESSAGE_LOCATOR = "//h3[@data-test=\"error\"]"; // (13.2) Add locator for the Error msg;
+
+    public InventoryPage(WebDriver driver) { // (13.3) Create an object using method LoginPage(WebDriver driver)
+        this.driver = driver;
+        userNameInput = driver.findElement(By.xpath(USERNAME_LOCATOR)); // (13.4) Put elements to the obj using locators.
+        passwordInput = driver.findElement(By.xpath(PASSWORD_LOCATOR));
+        button = driver.findElement(By.xpath(BUTTON_LOCATOR));
+//        message = driver.findElement(By.xpath(MESSAGE_LOCATOR));
+    }
+
+    private WebElement userNameInput; // (13.5) Create WebElements for the UserName input;
+    private WebElement passwordInput; // (13.5) Create WebElements for the Password input;
+    private WebElement button; // (13.5) Create WebElements for the Login button;
+    private WebElement message; // (13.5) Create WebElements for the Error message;
+
+    public void login(String username, String password) { // (13.6) Using login() method arrange login flow.
+        userNameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        button.click();
+    }
+
+    public boolean checkMessage(String str) {
+        message = driver.findElement(By.xpath(MESSAGE_LOCATOR));
+        return (message.getText().contains(str));
+    }
+}
