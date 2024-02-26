62c32a249ab253bf667d52759b940272b7b8d1f2
Andriy Glabay <rishelevsky@gmail.com>
2024-02-26 18:16:47 +0200

    InventoryPage.java: Implements the list of Inventory items.

--- src/test/java/com/example/pageobject/InventoryPage.java
+++ src/test/java/com/example/pageobject/InventoryPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 // (20) Copy and rename new test class: LoginPage.java -> InventoryPage.java
 // (20.0) Remove all unnecessary;
 // (20.1) Create locator for Inventory items;
@@ -11,6 +13,7 @@ import org.openqa.selenium.WebElement;
 public class InventoryPage {
     private WebDriver driver;
     private static final String INVENTORY_ITEMS_LOCATOR = "//*[@id='inventory_container']//div[@class='inventory_item]";
+    private List<WebElement> inventoryItems; // (20.2) Implements the list of Inventory items.
     public InventoryPage(WebDriver driver) {
         this.driver = driver;
     }
