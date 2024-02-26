2d7441a63e337daded59716a1c5c4c317455849a
Andriy Glabay <rishelevsky@gmail.com>
2024-02-26 19:11:56 +0200

    InventoryPage.java: Create locator for images.

--- src/test/java/com/example/pageobject/InventoryPage.java
+++ src/test/java/com/example/pageobject/InventoryPage.java
@@ -9,10 +9,13 @@ import java.util.List;
 // (20) Copy and rename new test class: LoginPage.java -> InventoryPage.java
 // (20.0) Remove all unnecessary;
 // (20.1) Create locator for Inventory items;
+// (21.4) Create locator for images.
 
 public class InventoryPage {
     private WebDriver driver;
     private static final String INVENTORY_ITEMS_LOCATOR = "//div[@id='inventory_container']//div[@class='inventory_item']";
+    private static final String INVENTORY_IMAGES_LOCATOR = "//div[@class='inventory_item']//img[@class='inventory_item_img']";
+
     private List<WebElement> inventoryItems; // (20.2) Implements the list of Inventory items.
     public InventoryPage(WebDriver driver) {
         this.driver = driver;
@@ -21,5 +24,9 @@ public class InventoryPage {
 
     public int getNumberOfInventoryItems() { // (20.4) Create the method.
         return inventoryItems.size(); // (20.5) get inventory items qty.
+    }
+
+    public List<String> getImageUrl() {
+
     }
 }
